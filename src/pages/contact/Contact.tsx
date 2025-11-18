51703f32799b6844f1935a2c68e004f190c5ccb7
Michael <michaelsagun014@gmail.com>
2025-11-18 16:31:17 +0800

    mobile responsiveness

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -42,7 +42,7 @@ const Contact = () => {
   return (
     <main className="space-y-5 pt-20">
       <header className="container mx-auto space-y-5 px-4 text-center">
-        <h2 className="font-solitreo text-center text-4xl font-bold text-(--red) md:text-5xl">
+        <h2 className="text-center text-4xl font-bold text-(--red) md:text-5xl">
           Comments<span className="text-black">?</span> Suggestions
           <span className="text-black">?</span> Need help<span className="text-black">?</span>
         </h2>
@@ -52,7 +52,7 @@ const Contact = () => {
 
       <div className="bg-gray-500 bg-[url(~/assets/chuck-eye.webp)] bg-cover bg-center bg-no-repeat bg-blend-overlay">
         <section className="container mx-auto space-y-10 p-10">
-          <div className="grid grid-cols-2 gap-10">
+          <div className="flex flex-col md:grid md:grid-cols-2 gap-10">
             <div className="card bg-base-100 w-full justify-evenly space-x-3 p-5 shadow-xl">
               <div className="border-success/50 absolute bottom-1/4 left-0 h-1/2 rounded border-r-4" />
 
@@ -60,10 +60,10 @@ const Contact = () => {
 
               <div className="h-1 w-24 bg-(--red)/50" />
 
-              <div className="mt-5 flex items-center justify-evenly">
+              <div className="mt-5 flex flex-row gap-3 items-center justify-evenly">
                 <div className="tooltip tooltip-bottom" data-tip="Chat with us">
                   <button
-                    className="btn btn-success btn-circle h-24 w-24 space-x-4 text-white"
+                    className="btn btn-success btn-circle lg:h-24 lg:w-24 space-x-4 text-white"
                     onClick={() => setContactOpen(true)}
                   >
                     <img src={Chat} className="h-[50%]" />
@@ -72,7 +72,7 @@ const Contact = () => {
 
                 <div className="tooltip tooltip-bottom" data-tip="Send us an email">
                   <button
-                    className="btn btn-success btn-circle h-24 w-24 space-x-4 text-white"
+                    className="btn btn-success btn-circle lg:h-24 lg:w-24 space-x-4 text-white"
                     onClick={() => window.open('mailto:sales@sunnyfoods.com.ph', '_blank')}
                   >
                     <img src={Email} className="h-[50%]" />
@@ -80,7 +80,7 @@ const Contact = () => {
                 </div>
 
                 <div className="tooltip tooltip-bottom" data-tip="Visit our Facebook page">
-                  <button className="btn btn-success btn-circle h-24 w-24 space-x-4 text-white">
+                  <button className="btn btn-success btn-circle lg:h-24 lg:w-24 space-x-4 text-white">
                     <img src={Facebook} className="h-[50%]" />
                   </button>
                 </div>
@@ -89,7 +89,21 @@ const Contact = () => {
               {renderModal()}
             </div>
 
-            <div className="card bg-base-100 row-span-2 flex w-full justify-center p-5 shadow-xl">
+            <div className="card bg-base-100 w-full p-5 shadow-xl row-span-2">
+              <div className="border-success/50 absolute bottom-1/4 left-0 h-1/2 rounded border-r-4" />
+
+              <p className="text-start text-xl font-bold">Visit our store location:</p>
+
+              <div className="h-1 w-24 bg-(--red)/50" />
+
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1625.4783164662417!2d121.03392743413092!3d14.307902529289628!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397d786cc958323%3A0x6426c3b99529899e!2sSUNNY%20FOODS%20INC.!5e0!3m2!1sen!2sph!4v1763358420686!5m2!1sen!2sph"
+                className="mt-5 h-full rounded-lg"
+                loading="lazy"
+              />
+            </div>
+
+            <div className="card bg-base-100 flex w-full justify-center p-5 shadow-xl">
               <div className="border-success/50 absolute bottom-1/4 left-0 h-1/2 rounded border-r-4" />
 
               <p className="text-xl font-bold">Send us your inquiries here:</p>
@@ -114,26 +128,13 @@ const Contact = () => {
                 <fieldset className="fieldset">
                   <p className="label text-lg font-bold">Message:</p>
                   <textarea
-                    className="textarea h-[18svh] w-full resize-none rounded-md"
+                    className="textarea w-full resize-none rounded-md"
                     placeholder="Leave a comment..."
                   />
                 </fieldset>
+                
                 <button className="btn btn-success rounded-md text-lg text-white w-full">Submit</button>
               </div>
-            </div>
-
-            <div className="card bg-base-100 w-full p-5 shadow-xl">
-              <div className="border-success/50 absolute bottom-1/4 left-0 h-1/2 rounded border-r-4" />
-
-              <p className="text-start text-xl font-bold">Visit our store location:</p>
-
-              <div className="h-1 w-24 bg-(--red)/50" />
-
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1625.4783164662417!2d121.03392743413092!3d14.307902529289628!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397d786cc958323%3A0x6426c3b99529899e!2sSUNNY%20FOODS%20INC.!5e0!3m2!1sen!2sph!4v1763358420686!5m2!1sen!2sph"
-                className="mt-5 h-[23svh] rounded-lg"
-                loading="lazy"
-              />
             </div>
           </div>
         </section>
