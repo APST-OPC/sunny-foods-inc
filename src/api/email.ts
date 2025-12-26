3e0766e71d754d8242c7e8d989254d72e281af3f
adriandpExt <152352852+adriandpExt@users.noreply.github.com>
2025-12-26 17:12:24 +0800

    email service

---
+++ src/api/email.ts
@@ -0,0 +1,21 @@
+import type { IContactForm } from '~/pages/contact/type';
+
+import axios from 'axios';
+import { buildEmailHtml } from '~/template/email-form';
+
+const URI = '/_api/v1/email-service';
+
+export const sendEmail = async (props: IContactForm): Promise<IContactForm> => {
+  const payload = {
+    from: 'Sunny Foods Web <noreply@chr.gg>',
+    to: 'Sunny Foods Sales <sales@sunnyfoods.com.ph>',
+    subject: `You have received a new inquiry from ${props.fullname}`,
+    body: {
+      html: buildEmailHtml(props),
+    },
+  };
+
+  const response = await axios.post(URI, [payload]);
+
+  return response.data;
+};
