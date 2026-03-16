5471daacc267c404e3ad188f15da7f85bf07604d
Michael <michaelsagun014@gmail.com>
2026-03-16 15:15:25 +0800

    fix: build errors

--- src/pages/contact/utils.tsx
+++ src/pages/contact/utils.tsx
@@ -1,34 +1,29 @@
-import type { IContactButton } from './type';
+import type { IContactUs } from "./type";
 
 import { FaViber } from 'react-icons/fa';
-import { AiOutlineFacebook, AiOutlineLinkedin } from 'react-icons/ai';
+import { AiOutlineFacebook } from 'react-icons/ai';
 import { PiTiktokLogo } from 'react-icons/pi';
 import { FaInstagram } from 'react-icons/fa6';
 
-export const contactButtons: IContactButton[] = [
+export const contactBtns: IContactUs[] = [
   {
-    contactIcon: ({ size }) => <FaViber size={size} color="#7360f2" />,
+    contactIcon: <FaViber size={60} color="#7360f2" />,
     contactCTA: 'Chat with us on Viber',
-    openWindow: () => window.open('viber://chat?number=%2B9392379999', '_blank'),
+    contactFn: () => window.open('viber://chat?number=%2B9392379999', '_blank'),
   },
   {
-    contactIcon: ({ size }) => <AiOutlineFacebook size={size} color="#1877f2" />,
+    contactIcon: <AiOutlineFacebook size={60} color="#1877f2" />,
     contactCTA: 'Visit our Facebook page',
-    openWindow: () => window.open('https://www.facebook.com/SUNNYFOODSINC.2025', '_blank'),
+    contactFn: () => window.open('https://www.facebook.com/SUNNYFOODSINC.2025', '_blank'),
   },
   {
-    contactIcon: ({ size }) => <FaInstagram size={size} color="#d300c5" />,
+    contactIcon: <FaInstagram size={60} color="#d300c5" />,
     contactCTA: 'Visit our Instagram',
-    openWindow: () => window.open('https://www.instagram.com/sunnyfoods.com.ph', '_blank'),
+    contactFn: () => window.open('https://www.instagram.com/sunnyfoods.com.ph', '_blank'),
   },
   {
-    contactIcon: ({ size }) => <PiTiktokLogo size={size} />,
+    contactIcon: <PiTiktokLogo size={60} />,
     contactCTA: 'Follow us on TikTok',
-    openWindow: () => window.open('https://www.tiktok.com/@sunnyfoodsinc', '_blank'),
-  },
-  {
-    contactIcon: ({ size }) => <AiOutlineLinkedin size={size} color="#1877f2" />,
-    contactCTA: 'Visit our LinkedIn page',
-    openWindow: () => window.open('https://www.linkedin.com/company/sunny-foods-inc', '_blank'),
+    contactFn: () => window.open('https://www.tiktok.com/@sunnyfoodsinc', '_blank'),
   },
 ];
