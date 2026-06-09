7aecf90f5ee94f857facb1195ef52119e6c16834
virgil-ferolino <gitMaleFerolino@gmail.com>
2026-06-09 14:39:38 +0800

    services, fix build error

--- src/pages/services/Services.tsx
+++ src/pages/services/Services.tsx
@@ -1,11 +1,12 @@
-// import { whyChooseProducts } from './utils';
-import { GiLaurelCrown } from "react-icons/gi";
-import { HiMiniStar } from "react-icons/hi2";
-import { TbMeat } from "react-icons/tb";
+import { GiLaurelCrown } from 'react-icons/gi';
+import { HiMiniStar } from 'react-icons/hi2';
+import { TbMeat } from 'react-icons/tb';
 
-import { cn } from "~/libs/cn";
+import CustomerService from '~/assets/services/customer-support.png';
 
-import { brandAndProducts } from "./utils";
+import { cn } from '~/libs/cn';
+
+import { brandAndProducts, wholesaleAndLogistics } from './utils';
 
 const ServiceHeader = () => {
   return (
@@ -32,79 +33,56 @@ const ServiceHeader = () => {
 
       <div
         className={cn(
-          "divider mx-auto w-40",
-          "before:bg-linear-to-l before:from-(--light-brown) before:to-white",
-          "after:bg-linear-to-r after:from-(--light-brown) after:to-white",
-        )}>
+          'divider mx-auto w-40',
+          'before:bg-linear-to-l before:from-(--light-brown) before:to-white',
+          'after:bg-linear-to-r after:from-(--light-brown) after:to-white'
+        )}
+      >
         <TbMeat className="h-16 w-16 text-(--light-brown)" />
       </div>
 
       <p className="mx-auto w-full max-w-2xl px-2 text-lg leading-6 text-gray-600">
-        Our services are designed to support every aspect of your business
-        operations, from custom meat orders and private labeling to packaging,
-        wholesale supply, and distribution support. With a focus on quality,
-        reliability, and efficiency, we help businesses streamline processes,
-        strengthen their brand presence, and deliver exceptional products to
-        their customers.
+        Sunny Foods delivers high-quality meat products paired with outstanding customer service.
+        Alongside our premium meat selections, we provide comprehensive services that enhance
+        quality, convenience, and customer satisfaction at every step.
       </p>
     </div>
   );
 };
 
-// const LegacyContent = () => {
-//   return (
-//     <div className="grid place-items-center gap-5 px-5 md:grid-cols-2 lg:grid-cols-3">
-//       {whyChooseProducts.map(({ title, description, image }, ids) => (
-//         <div
-//           key={ids}
-//           className="card bg-base-100 border-base-200 h-full w-full max-w-xs shadow-md transition-transform duration-300 hover:-translate-y-2 hover:shadow-lg md:max-w-md"
-//         >
-//           <div className="card-body items-center text-center">
-//             <div className="mb-4 text-5xl">
-//               <img src={image} alt={title.toLowerCase()} className="h-14 w-14" />
-//             </div>
-//             <h3 className="card-title text-xl font-bold">{title}</h3>
-//             <p className="text-base-content/80">{description}</p>
-//           </div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
 const BrandAndProductService = () => {
   return (
     <div className="space-y-10">
-      <h1 className="text-center text-4xl font-bold">
-        Brand & Product Development Service
-      </h1>
+      <h1 className="text-center text-4xl font-bold">Brand & Product Development Service</h1>
       {brandAndProducts.map((item, index) => (
         <div
           className={cn(
-            "flex w-full items-center gap-20",
-            index % 2 !== 0 && "flex-row-reverse",
+            'flex w-full flex-col items-center gap-5 lg:gap-10',
+            index % 2 !== 0 ? 'lg:flex-row-reverse' : 'lg:flex-row'
           )}
-          key={index}>
-          <div className="h-90 w-175 overflow-hidden rounded-xl">
-            <img
-              src={item.image}
-              alt="packaging and labeling"
-              className="h-full w-full"
-            />
+          key={index}
+        >
+          <div
+            className="aspect-16/10 w-full overflow-hidden lg:h-100 lg:w-190 lg:rounded-xl"
+            data-aos={index % 2 !== 0 ? 'fade-left' : 'fade-right'}
+          >
+            <img src={item.image} alt="packaging and labeling" className="h-full w-full" />
           </div>
-          <div className="w-full space-y-5">
-            <h1 className="text-5xl font-semibold tracking-tight text-(--warm-red) md:text-6xl lg:text-7xl">
+          <div className="w-full space-y-3 px-5 lg:w-2/3">
+            <h1 className="text-center text-4xl font-semibold tracking-tight text-(--warm-red) md:text-5xl lg:text-start lg:text-6xl">
               {item.title}
             </h1>
-            <h1 className="text-lg">{item.description}</h1>
-
-            <ul className="list-inside list-disc space-y-1">
-              {item.bullet.map((data, ids) => (
-                <li className="text-lg" key={ids}>
-                  {data}
-                </li>
-              ))}
-            </ul>
+            <div className="space-y-2">
+              <h1 className="text-center text-base lg:text-start lg:text-lg">{item.description}</h1>
+              <div className="divider hidden lg:block" />
+              <ul className="hidden list-inside list-disc lg:block">
+                {item.bullet.map((data, ids) => (
+                  <li className="text-base lg:text-lg" key={ids}>
+                    {data}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       ))}
@@ -112,13 +90,62 @@ const BrandAndProductService = () => {
   );
 };
 
+const LogisticsAndSupply = () => {
+  return (
+    <div className="space-y-10">
+      <h1 className="text-center text-4xl font-bold">Wholesale, Logistics & Distribution</h1>
+
+      <div className="flex flex-col md:flex-row">
+        {wholesaleAndLogistics.map((item, index) => (
+          <div
+            key={index}
+            className="h-full w-full cursor-pointer space-y-5 p-5 shadow-black/25 hover:scale-105 hover:bg-(--light-brown)/20"
+          >
+            <div className="relative">
+              <img
+                src={item.image}
+                className="relative h-full w-full transition-all duration-300 md:rounded-xl"
+                alt={item.title}
+              />
+            </div>
+            <div className="h-full">
+              <h1 className="text-center text-4xl font-semibold tracking-tighter text-(--warm-red) lg:text-start">
+                {item.title}
+              </h1>
+              <p className="mt-3 text-center text-lg md:text-start">{item.description}</p>
+            </div>
+            <ul className="list-inside list-disc text-lg">
+              {item.bullet.map((data, index) => (
+                <li key={index}>{data}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const BusinessSupportService = () => {
+  return (
+    <div className="w-full space-y-5">
+      <h1 className="text-center text-4xl font-bold">Business Support Services</h1>
+
+      <img src={CustomerService} className="h-full w-full rounded-xl" alt="customer-service" />
+    </div>
+  );
+};
+
 const Services = () => {
   return (
-    <section className="container mx-auto space-y-20 py-20">
+    <section className="container mx-auto space-y-20 px-5 py-20">
       <ServiceHeader />
       <div className="divider m-10 mx-auto h-5 w-4/5 before:bg-linear-to-r before:from-[#F9F5F1] before:via-(--red) before:to-(--red) after:bg-linear-to-l after:from-[#F9F5F1] after:via-(--red) after:to-(--red)" />
-      {/* <LegacyContent /> */}
       <BrandAndProductService />
+      <div className="divider m-10 mx-auto h-5 w-4/5 before:bg-linear-to-r before:from-[#F9F5F1] before:via-(--red) before:to-(--red) after:bg-linear-to-l after:from-[#F9F5F1] after:via-(--red) after:to-(--red)" />
+      <LogisticsAndSupply />
+      <div className="divider m-10 mx-auto h-5 w-4/5 before:bg-linear-to-r before:from-[#F9F5F1] before:via-(--red) before:to-(--red) after:bg-linear-to-l after:from-[#F9F5F1] after:via-(--red) after:to-(--red)" />
+      <BusinessSupportService />
     </section>
   );
 };
