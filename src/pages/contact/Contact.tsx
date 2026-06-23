a181b77c5d7dd31a8253797fef3ec4ec550b7f83
Michael <michaelsagun014@gmail.com>
2026-06-23 13:52:49 +0800

    style: slight change in styling for the location section

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -208,7 +208,7 @@ const Contact = (): ReactElement => {
               title="maps"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1625.4783164662417!2d121.03392743413092!3d14.307902529289628!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397d786cc958323%3A0x6426c3b99529899e!2sSUNNY%20FOODS%20INC.!5e0!3m2!1sen!2sph!4v1763358420686!5m2!1sen!2sph"
               className={cn(
-                "md:h-[90%] w-full rounded-lg border-2 border-gray-400",
+                "lg:h-[90%] w-full rounded-lg border-2 border-gray-400",
                 loading && "skeleton",
               )}
               loading="lazy"
